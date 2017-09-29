bdff9418120b9eff93e3bb979c5d7b6b6c91c667
decemberrain21 <decemberrain21@gmail.com>
2017-09-29 23:46:18 +0630

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 app.get('/', function (req, res) {
-  res.send('Hello World test!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
