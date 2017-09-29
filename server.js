de61bbb7bdc56872bdd447cd79f9baf5d9c89411
decemberrain21 <decemberrain21@gmail.com>
2017-09-29 23:54:09 +0630

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
