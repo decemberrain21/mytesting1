5f30e97506104201b21021eaee23e416b26678fc
decemberrain21 <decemberrain21@gmail.com>
2017-09-30 00:51:29 +0630

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
+app.get('/user.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'user.html'));
+});
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
   console.log('hello..');
